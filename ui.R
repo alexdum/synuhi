c2e689865ec0fccdd0013714d0b6c970e60af596
alexdum <alexandru.dumitrescu@gmail.com>
2024-02-12 20:40:25 +0200

    split season year

--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@ ui <-
       sidebar = sidebar(
         selectInput("city", "City:", names_cities, selected = names_cities[sample(1:length(names_cities), 1)]),
         selectInput("season", "Season:", seasons, selected = seasons[1]),
+        sliderInput("year", "Year:", min = 2017, max = 2022, value = 2017),
         selectInput(
           "timeday", "Time of day:", 
           c("Day" = "day", "Night" = "night"))
