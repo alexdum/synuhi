0675293c2f87f99455f38b723d1ca820c67bb78f
alexdum <alexandru.dumitrescu@gmail.com>
2024-03-20 15:33:30 +0200

    improve code

--- global.R
+++ global.R
@@ -6,22 +6,16 @@ library(sf)
 library(dplyr)
 library(seas)
 library(highcharter)
+library(RColorBrewer)
 
 source("utils/leaflet_fun.R", local = T)
+source("utils/map_cols_fun.R", local = T)
 
 
 cities <- st_read("www/data/shp/cities_merged.geojson", quiet = T) |> arrange(name) |> filter(name != "Constanta")
-
 names_cities <- setNames(cities$cod, cities$name)
 
 
-day <- rast("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_intensity_day.nc")
-time(day[[3]]) <- as.Date("2017-01-15") # ajustare timp pentru vizualizare
-night <- rast("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_intensity_night.nc")
-
 seasons <-  mkseas(time(day), "DJF") |> unique() |> as.character()
-
 seasons <- setNames(seasons, seasons)
-domain_suhi <- c(-10, 10)
-pal_rev_suhi <- colorNumeric("RdYlBu", domain = domain_suhi, reverse = F, na.color = "transparent")
-pal_suhi <- colorNumeric("RdYlBu", domain = domain_suhi, reverse = T, na.color = "transparent")
+
