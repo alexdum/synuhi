ea698f16b61b23a84054a526a99b3b47b1f863bb
alexdum <alexandru.dumitrescu@gmail.com>
2024-04-12 21:28:12 +0300

    tidy code

--- sections/server_adaptation.R
+++ sections/server_adaptation.R
@@ -1,24 +1,24 @@
-data_selvar <- reactive({
+data_seladapt <- reactive({
   
   # subseteaza limita city
-  city_sub <- cities |> filter(cod %in% input$city_var)
-  city_sub_buf <- cities_buf |> filter(cod %in% input$city_var)
+  city_sub <- cities |> filter(cod %in% input$city_adapt)
+  city_sub_buf <- cities_buf |> filter(cod %in% input$city_adapt)
   
   # citeste fisier raster
   if (input$scenario %in% "orig") {
-    r <- rast(paste0("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_",input$timeday_var,".nc"))
+    r <- rast(paste0("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_",input$timeday_adapt,".nc"))
     time(r[[3]]) <- as.Date("2017-01-15") # ajustare timp pentru vizualizare
     
   } else {
-    r <- rast(paste0("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_",input$timeday_var,"_", input$scenario,".nc"))
-    time(r[[3]]) <- as.Date("2017-01-15") # ajustare timp pentru vizualizare
+    r <- rast(paste0("www/data/ncs/SENTINEL3B_SLSTR_L3C_0.01_SUHI_",input$timeday_adapt,"_", input$scenario,".nc"))
+  
     
   }
   
   
   
   # subsetare raster pentru harta
-  ri <- r[[which(paste(format(time(r), "%Y"), mkseas(time(r), "DJF")) %in% paste(input$year_var, input$season_var))]]
+  ri <- r[[which(paste(format(time(r), "%Y"), mkseas(time(r), "DJF")) %in% paste(input$year_adapt, input$season_adapt))]]
   
   
   domain_r <- c(-10, 10)
@@ -37,23 +37,25 @@ data_selvar <- reactive({
 
 
 # functie leaflet de start
-output$map_var <- renderLeaflet({
+output$map_adapt <- renderLeaflet({
   
   leaflet_fun()
 })
 
 observe({
   
-  rr <- data_selvar()$ri
-  data <- data_selvar()$city_sub_buf
-  pal <- data_selvar()$pal
-  pal_rev <- data_selvar()$pal_rev
-  domain_r <- data_selvar()$domain_r
+
+  
+  rr <- data_seladapt()$ri
+  data <- data_seladapt()$city_sub_buf
+  pal <- data_seladapt()$pal
+  pal_rev <- data_seladapt()$pal_rev
+  domain_r <- data_seladapt()$domain_r
   # pentru zoom retea observatii vizualizata
   bbox <- st_bbox(data) |> as.vector()
   
   
-  leafletProxy("map_var") |>
+  leafletProxy("map_adapt", data = data) |>
     addPolygons(
       data =  cities,
       color = "#252525", weight = 2, smoothFactor = 0.5,
@@ -68,29 +70,26 @@ observe({
       group = "Cities buffer") |>
     clearImages() |>
     addRasterImage(
-      rr, colors = pal, opacity = input$transp_var,
+      rr, colors = pal, opacity = input$transp_adapt,
       group = "SUHI") |> 
     clearControls() |>
     addLegend(
       title =  "SUHI [°C]",
       position = "bottomright",
       pal = pal_rev, values = domain_r,
-      opacity = input$transp_var,
+      opacity = input$transp_adapt,
       labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE))) |>
     fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
   
-  print(bbox)
-  
 })
 
-# variabile grafic
-chart_vars2 <- reactiveValues(df = NULL, coordinates = NULL)
+# adaptiabile grafic
+chart_adapts2 <- reactiveValues(df = NULL, coordinates = NULL)
 
 # pentru actualizare grafice cu schimbare oras
-observeEvent(input$city_var,{ 
-  city_sub_buf <- data_selvar()$city_sub_buf
+observeEvent(input$city_adapt,{ 
+  city_sub_buf <- data_seladapt()$city_sub_buf
   cent <- st_centroid(city_sub_buf) |> st_coordinates()
   co <- data.frame(lng = cent[1,1], lat = cent[1,2])
-  chart_vars2$coordinates <- co
-  print( chart_vars2$coordinates)
+  chart_adapts2$coordinates <- co
 })
